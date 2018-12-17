159136a3e201b7cb3521092376b24f3ed5ada068
Robinson Castro F <r.a.c.f.28@gmail.com>
2018-12-17 20:23:08 -0300

    usub

--- usub.py
+++ usub.py
@@ -1,6 +1,11 @@
+from uadd import uadd
+
+
 def usub(a, b, ans):
     n = len(a)
     i = n-1
-    while b[i] == 0:
-
+    while i >= 0 and b[i] == 0:
+        b[i] = -1
         i -= 1
+    b[i] ^= 2**32 - 1
+    uadd(a, b, ans)
